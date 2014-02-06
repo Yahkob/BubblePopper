e63311be85fcbd56305f9d377e0e017e972082a8
digilord <dmorrigan@aeonstructure.com>
2014-02-06 13:38:26 -0700

    Code cleanup. Name normalization.

--- client/client.js
+++ client/client.js
@@ -1,43 +1,47 @@
 Deps.autorun(function(){
-  gameSubscription = Meteor.subscribe("myGames",Meteor.userId());
-  });
+  Meteor.subscribe("games", Meteor.userId());
+  Meteor.subscribe("Bubbles");
+});
+/////////////////////////////////////////
+// home Template
+/////////////////////////////////////////
+Template.home.helpers({
+  game: function(){
+    return Games.findOne({current: true});
+  }
+});
 
-
-Template.home.game = function(){
-  return gameCollection.findOne({current: true});
-};
 Template.home.events({
   "click #newGame" : function() {
     Meteor.call('newGame')
   },
   "click #finishGame": function(){
-    var game = gameCollection.findOne({current: true});
+    var game = Games.findOne({current: true});
     Meteor.call('finishGame', game._id);
     }
-  });
-
-Deps.autorun(function(){
-  Meteor.subscribe("bubbles");
 });
 
- Template.grid.buttons = function (){
+/////////////////////////////////////////
+// grid Template
+/////////////////////////////////////////
+Template.grid.helpers({
+  buttons: function (){
     var user = Meteor.user();
     if(!user){
         return;
     }
-    var game = gameCollection.findOne({players: {$in: [user._id]}, current: true});  
+    var game = Games.findOne({players: {$in: [user._id]}, current: true});
     if(!game){
         return;
     }
-    return bubbles.find({gameId: game._id});
-}
+    return Bubbles.find({gameId: game._id});
+  }
+});
 
+Template.grid.events({
+  'click .button': function(ev) {
+    $(ev.target).css('visibility', 'hidden');
 
-
-  Template.grid.events({
-    'click .button': function(ev) {
-      $(ev.target).css('visibility', 'hidden');
-      
-   }
+  }
 });
 
--- server/server.js
+++ server/server.js
@@ -1,74 +1,68 @@
-  
-  Meteor.methods({
-    newGame: function() {
-      allocateGame(this.userId);
-    },
-    finishGame: function(_id) {
-      gameCollection.update({_id: _id}, {$set: {active: false, finished: true}});
-    }
+
+Meteor.methods({
+  newGame: function() {
+    allocateGame(this.userId);
+  },
+  finishGame: function(_id) {
+    Games.update({_id: _id}, {$set: {active: false, finished: true}});
+  }
+})
+
+Meteor._onLogin  = function (userId) {
+  allocateGame(userId);
+  console.log("user: " + userId + "just logged in.")
+}
+
+Meteor._onLogout = function (userId) {
+  leaveGames(userId);
+  console.log("user: " + userId + "just logged out.")
+}
+
+
+allocateGame = function(userId) {
+  var gameWaiting = Games.findOne({players: {$size: 1}, current: true});
+
+  if (!gameWaiting) {
+    console.log("creating a new game, none available");
+    var gameId = Games.insert({players: [userId], active: false, finished: false, current: true});
+
+    // Create 64 bubbles for this game
+    _.times(64, function(n){
+      console.log("creating bubble grid")
+      Bubbles.insert({gameId: gameId});
+    });
+
+  } else {
+    console.log("connecting with an existing waiting player");
+    Games.update({_id: gameWaiting._id}, {$set: {active: true}, $push: {players: userId}});
+  }
+};
+
+leaveGames = function(userId) {
+  console.log("leaving all unfinished games");
+
+  // Why are you removing the game?
+  Games.remove({$and: [{players: userId, players: {$size: 1}}]});
+
+  // Then trying to find the games
+  var games = Games.find({$and: [{players: userId, active: true}]});
+
+  // Then trying to update the games you removed on line 45
+  games.forEach(function(game) {
+    Games.update({_id: game._id}, {$set: {active: false, finished: true}});
   })
-  
-  Meteor._onLogin  = function (userId) { 
-    allocateGame(userId);
-    console.log("user: " + userId + "just logged in.") 
-  }
-  
-  Meteor._onLogout = function (userId) { 
-    leaveGames(userId);
-    console.log("user: " + userId + "just logged out.") 
-  }
-  
-      
-    
-  allocateGame = function(userId) {
-    var gameWaiting = gameCollection.findOne({players: {$size: 1}, current: true});
-    
-    if (!gameWaiting) {
-     
-      console.log("creating a new game, none available");
-      //gameCollection.update({players: userId}, {$set: {current: false}}, {multi: true});
-      var gameId = gameCollection.insert({players: [userId], active: false, finished: false, current:                         true});
-      console.log("inserting players")
-      _.times(64, function(n){
-          bubbles.insert({gameId: gameId}); 
-          console.log("creating bubble grid")
-      });
-    } else {
-      console.log("connecting with an existing waiting player");
-      gameCollection.update({_id: gameWaiting._id}, {$set: {active: true}, $push: {players: userId}});
-    }
-  };
-  
-  leaveGames = function(userId) {
-    console.log("leaving all unfinished games");
-    
-    gameCollection.remove({$and: [{players: userId, players: {$size: 1}}]});
-    
-    var games = gameCollection.find({$and: [{players: userId, active: true}]});
-  
-    games.forEach(function(game) {
-      gameCollection.update({_id: game._id}, {$set: {active: false, finished: true}});          
-    })
-  };
-  
-  Meteor.publish('myGames', function() {
-   
-    if (!this.userId) {
-        console.log('No users.');
-        //return [];
-        this.ready();
-    } else {
-      console.log('Publishing user: '+this.userId);
-      return gameCollection.find({players: this.userId});
-    }
-  
-  });
+};
+
+Meteor.publish('games', function(userId) {
+  if(userId)
+    return Games.find({players: this.userId});
+});
 
 
-  Meteor.publish('bubbles', function(list){
-    return bubbles.find({grid : {$in: list}});
-  });
-  
+Meteor.publish('bubbles', function(){
+  return Bubbles.find();
+});
+
 
 
 
