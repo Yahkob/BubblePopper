d979f21a85cd43bd896875dba3fddddd0d552502
Yahkob <jakobharclerode@gmail.com>
2014-02-16 00:36:24 -0700

    Code formatting

--- server/server.js
+++ server/server.js
@@ -20,27 +20,27 @@ Meteor._onLogout = function (userId) {
   console.log("user: " + userId + "just logged out.")
 }
 
-
 allocateGame = function(userId) {
-    var gameWaiting = Games.findOne({players: {$size: 1}, current: true});
-
-    if (!gameWaiting) {
-      console.log("creating a new game, none available");
-      Games.update({players: userId}, {$set: {current: false}}, {multi: true});
-      var gameId = Games.insert({players: [userId], active: false, finished: false, current: true});
-       _.times(64, function(n){
-      Bubbles.insert({gameId: gameId});
+  var gameWaiting = Games.findOne({players: {$size: 1}, current: true});
+  if (!gameWaiting) {
+    console.log("creating a new game, none available");
+    Games.update({players: userId}, {$set: {current: false}}, {multi: true});
+    var gameId = Games.insert({players: [userId], active: false, finished: false, current: true});
+     _.times(64, function(n){
+    Bubbles.insert({gameId: gameId});
     });
-    } else {
-      if (_.contains(gameWaiting.players, userId)) {
-        console.log("Cannot play with yourself. But you can stay waiting in the game.");
-      } else {
-
-        console.log("connecting with an existing waiting player");
-        Games.update({_id: gameWaiting._id}, {$set: {active: true}, $push: {players: userId}});
+  }
+  else {
+    if (_.contains(gameWaiting.players, userId)){
+      console.log("No other users are currently in the lobby please wait.");
+    }
+    else{
+      console.log("connecting with an existing waiting player");
+      Games.update({_id: gameWaiting._id}, {$set: {active: true}, $push: {players: userId}});
     }
   }
 };
+
 leaveGames = function(userId) {
   console.log("leaving all unfinished games");
   Games.remove({$and: [{players: userId, players: {$size: 1}}]});
